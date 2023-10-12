7b5a2ad5d5a04f5e61991d2c2174c66f7ef658b0
AmerDDD <you@example.com>
2023-10-12 16:06:04 +0200

    adidng files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+import static java.lang.Integer.sum;
+
+// for strings String text = reader.nextLine();
+// for integers int number = Integer.parseInt(reader.nextLine())
+// logical operators
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+    }
+}
